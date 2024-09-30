79e023405bc80ee330e88d2c531c5c52f75993e5
hyeon12 <seojh912_@naver.com>
2024-09-30 15:30:58 +0900

    Experience 구성

--- src/app/main/components/Experience.js
+++ src/app/main/components/Experience.js
@@ -1,10 +1,43 @@
 import React from 'react';
 import { Section } from 'react-fullpage';
+import styled from 'styled-components';
+import Item from './Item';
+import { FaReact } from 'react-icons/fa';
+
+const Wrapper = styled.div`
+  &.first {
+    padding-top: 51px;
+  }
+
+  display: flex;
+  flex-wrap: wrap;
+
+  .item {
+    width: 0;
+    flex-grow: 1;
+    margin: 20px 0;
+  }
+
+  .item + .item {
+    margin-left: 20px;
+  }
+`;
 
 const Experience = () => {
   return (
     <Section>
-      <h1>Education & Work Experience</h1>
+      <Wrapper className="layout-width first">
+        <Item title="EDUCATION"></Item>
+        <Item title="WORK EXPERIENCE"></Item>
+      </Wrapper>
+      <Wrapper className="layout-width">
+        <Item title="CERTIFICATE"></Item>
+        <Item title="SKILL">
+          <div>
+            <FaReact />
+          </div>
+        </Item>
+      </Wrapper>
     </Section>
   );
 };
